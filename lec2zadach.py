7a12e60f7e2e19e896e036be552f0ff9dbb64f7c
ArtemB2 <114933914+ArtemB2@users.noreply.github.com>
2023-10-05 16:18:49 +0000

    lec2

---
+++ lec2zadach.py
@@ -0,0 +1,11 @@
+a='Hello, World!'
+print(a)
+
+b='Маша + Петя = Любовь'
+print(b)
+
+c = 3
+d = 4
+print(f'x={c}+{d}')
+
+print(f'x={c+d}')
---
+++ lec_2_strink.py
@@ -0,0 +1,13 @@
+a = 'good'
+b = 'job'
+
+print(a + b)
+
+print(a * 3)
+
+с = 50 
+d = 10 
+print(f'вставим число {с} и {d}')
+
+
+print(len(a))
